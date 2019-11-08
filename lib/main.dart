49adccb90cf750c7097a2358807e4745c4333687
Bành Thanh Sơn <takashato@gmail.com>
2019-11-08 22:18:04 +0700

    Modify flutter_base

--- lib/main.dart
+++ lib/main.dart
@@ -1,76 +1,237 @@
+import 'dart:developer';
+import 'dart:io';
+import 'dart:math';
+
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_database/firebase_database.dart';
+import 'package:firebase_database/ui/firebase_animated_list.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'auth.dart';
+import 'package:flutter/services.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:google_sign_in/google_sign_in.dart' as prefix0;
+import 'package:image_picker/image_picker.dart';
 
-void main() => runApp(MyApp());
+final ThemeData kIOSTheme = new ThemeData(
+  primarySwatch: Colors.orange,
+  primaryColor: Colors.grey[100],
+  primaryColorBrightness: Brightness.light,
+);
 
-class MyApp extends StatelessWidget {
+final ThemeData kDefaultTheme = new ThemeData(
+  primarySwatch: Colors.blue,
+  accentColor: Colors.blueAccent[400],
+);
+
+void main() => runApp(FriendlychatApp());
+
+final googleSignIn = new GoogleSignIn();
+final auth = FirebaseAuth.instance;
+final reference = FirebaseDatabase.instance.reference().child('flutter-chat-messages');
+
+Future<bool> _ensureLoggedIn() async {
+  GoogleSignInAccount user = googleSignIn.currentUser;
+  if (user == null) {
+    user = await googleSignIn.signInSilently();
+  }
+  if (user == null) {
+    user = await googleSignIn.signIn();
+
+  }
+  final GoogleSignInAuthentication googleSignInAuthentication =
+      await user.authentication;
+
+  final AuthCredential credential = GoogleAuthProvider.getCredential(
+    accessToken: googleSignInAuthentication.accessToken,
+    idToken: googleSignInAuthentication.idToken,
+  );
+
+  final AuthResult authResult = await auth.signInWithCredential(credential);
+  final FirebaseUser fuser = authResult.user;
+
+  if (fuser.isAnonymous) return false;
+  if (await fuser.getIdToken() == null) return false;
+  final FirebaseUser currentUser = await auth.currentUser();
+  if (fuser.uid != currentUser.uid) return false;
+  return true;
+}
+
+class FriendlychatApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'FlutterBase',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Flutterbase'),
-          backgroundColor: Colors.amber,
-        ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[LoginButton(), UserProfile()],
-          ),
+      title: 'FriendlyChat with ❤',
+      theme: defaultTargetPlatform == TargetPlatform.iOS
+          ? kIOSTheme
+          : kDefaultTheme,
+      home: new ChatScreen(),
+    );
+  }
+}
+
+@override
+class ChatMessage extends StatelessWidget {
+  ChatMessage({this.snapshot, this.animation});
+  final DataSnapshot snapshot;
+  final Animation animation;
+
+  Widget build(BuildContext context) {
+    return new SizeTransition(
+      sizeFactor: new CurvedAnimation(
+          parent: animation, curve: Curves.easeOut),
+      axisAlignment: 0.0,
+      child: new Container(
+        margin: const EdgeInsets.symmetric(vertical: 10.0),
+        child: new Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            new Container(
+              margin: const EdgeInsets.only(right: 16.0),
+              child: new CircleAvatar(backgroundImage: new NetworkImage(snapshot.value['senderPhotoUrl'])),
+            ),
+            new Expanded(
+              child: new Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  new Text(
+                      snapshot.value['senderName'],
+                      style: Theme.of(context).textTheme.subhead),
+                  new Container(
+                    margin: const EdgeInsets.only(top: 5.0),
+                    child: snapshot.value['imageUrl'] != null ?
+                    new Image.network(
+                      snapshot.value['imageUrl'],
+                      width: 250.0,
+                    ) :
+                    new Text(snapshot.value['text']),
+                  ),
+                ],
+              ),
+            ),
+          ],
         ),
       ),
     );
   }
 }
 
-class UserProfile extends StatefulWidget {
+class ChatScreen extends StatefulWidget {
   @override
-  UserProfileState createState() => UserProfileState();
+  State createState() => new ChatScreenState();
 }
 
-class UserProfileState extends State<UserProfile> {
-  Map<String, dynamic> _profile;
-  bool _loading = false;
-
-  @override
-  initState() {
-    super.initState();
-    authService.profile.listen((state) => setState(() => _profile = state));
-
-    authService.loading.listen((state) => setState(() => _loading = state));
-  }
+class ChatScreenState extends State<ChatScreen> {
+  final TextEditingController _textController = new TextEditingController();
+  bool _isComposing = false;
 
   @override
   Widget build(BuildContext context) {
-    return Column(children: <Widget>[
-      Container(padding: EdgeInsets.all(20), child: Text(_profile.toString())),
-      Container(padding:EdgeInsets.all(20), child: Text('Loading: ${_loading.toString()}')),
-    ]);
+    return new Scaffold(
+        appBar: new AppBar(
+          title: new Text("FriendlyChat with ❤"),
+          elevation:
+              Theme.of(context).platform == TargetPlatform.iOS ? 0.0 : 4.0,
+        ),
+        body: new Column(children: <Widget>[
+          new Flexible(
+            child: new FirebaseAnimatedList(
+              query: reference,
+              sort: (a, b) => b.key.compareTo(a.key),
+              padding: new EdgeInsets.all(8.0),
+              reverse: true,
+              itemBuilder: (_, DataSnapshot snapshot, Animation<double> animation, int unknown) {
+                return new ChatMessage(
+                    snapshot: snapshot,
+                    animation: animation
+                );
+              },
+            ),
+          ),
+          new Divider(height: 1.0),
+          new Container(
+            decoration: new BoxDecoration(color: Theme.of(context).cardColor),
+            child: _buildTextComposer(),
+          )
+        ]));
   }
-}
 
-class LoginButton extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return StreamBuilder(
-        stream: authService.user,
-        builder: (context, snapshot) {
-          if (snapshot.hasData) {
-            return MaterialButton(
-              onPressed: () => authService.signOut(),
-              color: Colors.red,
-              textColor: Colors.white,
-              child: Text('Signout'),
-            );
-          } else {
-            return MaterialButton(
-              onPressed: () => authService.googleSignIn(),
-              color: Colors.white,
-              textColor: Colors.black,
-              child: Text('Login with Google'),
-            );
-          }
-        });
+  Widget _buildTextComposer() {
+    return new IconTheme(
+      data: new IconThemeData(color: Theme.of(context).accentColor),
+      child: new Container(
+          margin: const EdgeInsets.symmetric(horizontal: 8.0),
+          child: new Row(children: <Widget>[
+            new Container(
+              margin: new EdgeInsets.symmetric(horizontal: 4.0),
+              child: new IconButton(
+                  icon: new Icon(Icons.photo_camera),
+                  onPressed: () async {
+                    if (!(await _ensureLoggedIn())) return;
+                    File imageFile = await ImagePicker.pickImage(source: ImageSource.gallery);
+                    int random = new Random().nextInt(100000);
+                    StorageReference ref =
+                    FirebaseStorage.instance.ref().child("image_$random.jpg");
+                    StorageUploadTask uploadTask = ref.putFile(imageFile);
+                    StorageTaskSnapshot snapShot = await uploadTask.onComplete;
+                    String downloadUrl = await snapShot.ref.getDownloadURL();
+                    _sendMessage(imageUrl: downloadUrl);
+                  }),
+            ),
+            new Flexible(
+              child: new TextField(
+                controller: _textController,
+                onChanged: (String text) {
+                  setState(() {
+                    _isComposing = text.length > 0;
+                  });
+                },
+                onSubmitted: _handleSubmitted,
+                decoration:
+                    new InputDecoration.collapsed(hintText: "Gửi tin nhắn"),
+              ),
+            ),
+            new Container(
+                margin: new EdgeInsets.symmetric(horizontal: 4.0),
+                child: Theme.of(context).platform == TargetPlatform.iOS
+                    ? new CupertinoButton(
+                  child: new Text("Gửi"),
+                  onPressed: _isComposing
+                      ? () => _handleSubmitted(_textController.text)
+                      : null,
+                )
+                    : new IconButton(
+                  icon: new Icon(Icons.send),
+                  onPressed: _isComposing
+                      ? () => _handleSubmitted(_textController.text)
+                      : null,
+                )),
+          ]),
+          decoration: Theme.of(context).platform == TargetPlatform.iOS
+              ? new BoxDecoration(
+                  border:
+                      new Border(top: new BorderSide(color: Colors.grey[200])))
+              : null),
+    );
+  }
+
+  Future<Null> _handleSubmitted(String text) async {
+    _textController.clear();
+    setState(() {
+      _isComposing = false;
+    });
+    if (!(await _ensureLoggedIn())) return;
+    _sendMessage(text: text);
+  }
+
+  void _sendMessage({ String text, String imageUrl }) {
+    reference.push().set({
+      'text': text,
+      'imageUrl': imageUrl,
+      'senderName': googleSignIn.currentUser.displayName,
+      'senderPhotoUrl': googleSignIn.currentUser.photoUrl,
+    });
+//    analytics.logEvent(name: 'send_message');
   }
 }
